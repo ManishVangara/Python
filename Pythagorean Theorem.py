7251fd539d72d1cee9f61760625093cf992c342c
ManishVangara <86878532+ManishVangara@users.noreply.github.com>
2022-02-16 23:11:11 +0530

    Update Pythagorean Theorem.py

--- Pythagorean Theorem.py
+++ Pythagorean Theorem.py
@@ -9,6 +9,8 @@ def is_a_right_triangle(a, b, c):
         return c ** 2 == a ** 2 + b ** 2
     if a > b and a > c:
         return a ** 2 == b ** 2 + c ** 2
+    if b > a and b > c :
+        return b ** 2 = a ** 2 + c ** 2 
 
 
 print(is_a_right_triangle(5, 3, 4))
